bd76626e34ee17a1bb8a506ac9261ec540126a8a
mendezcaesar <319345585+mendezcaesar@users.noreply.github.com>
2026-08-30 17:47:35 -0500

    chore: configure local environment properties with modern supabase publishable api token

---
+++ logos-app/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://mtjgazaflgtgdbeckgnx.supabase.co',
+    supabaseKey: 'sb_publishable__JdboXqAK85dvRVdVe_F1A_TtPQyZ9s'
+  };
+  
